2e440c3b22347aafdc1cdfe40b9a322abbbe414d
Kerim Sever <ks835@njit.edu>
2021-12-13 12:23:55 -0500

    another test

--- project.py
+++ project.py
@@ -5,3 +5,16 @@ def split_and_join(line):
  
 line="this is a string"
 print(split_and_join(line))
+
+
+
+
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+
