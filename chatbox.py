5e3872b52afce49214eec4164a05b7f53c3009ca
Kerim Sever <55418044+Kerims23@users.noreply.github.com>
2021-12-24 09:42:07 -0500

    Update chatbox.py

--- chatbox.py
+++ chatbox.py
@@ -1,2 +1,3 @@
 from chatterbot import Chatbot
 from chatterbot.trainers import list_trainer from chatterbot.trainers import chatter_bot_corpus_trainer
+my_bot Chat_bot (names "Pybot, read only-True, logic adapters=)
