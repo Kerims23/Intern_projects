27eb2cc01e38e3dd0fbfc2ad9f8e7d5c755923ca
Kerim Sever <ks835@njit.edu>
2021-12-25 12:22:07 -0500

    testing some stuff

--- chatbox.py
+++ chatbox.py
@@ -1,3 +1,4 @@
 from chatterbot import Chatbot
 from chatterbot.trainers import list_trainer from chatterbot.trainers import chatter_bot_corpus_trainer
-my_bot Chat_bot (names "Pybot, read only-True, logic adapters=")
+my_bot Chat_bot (names "PyBot, read only-True, logic adapters= ['chatterbot.logic.Mathmatical Evalution", 'chatterbot.logic.BestMatch'])
+
