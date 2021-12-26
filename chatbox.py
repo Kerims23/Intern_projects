482d1cc9687cc06a0b776fc5f5bc2fc4ec9ba0ff
Kerim Sever <ks835@njit.edu>
2021-12-26 10:01:28 -0500

    small talk

--- chatbox.py
+++ chatbox.py
@@ -2,3 +2,4 @@ from chatterbot import Chatbot
 from chatterbot.trainers import list_trainer from chatterbot.trainers import chatter_bot_corpus_trainer
 my_bot Chat_bot (names "PyBot, read only-True, logic adapters= ['chatterbot.logic.Mathmatical Evalution", 'chatterbot.logic.BestMatch'])
 
+small_talk['hi there!', "how are you?", "what are you doing today", "glad to hear your doing good", "we should grab lunch sometime."]
