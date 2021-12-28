5e6c761f821df545a169ebff5ad7283635c72960
Kerim Sever <ks835@njit.edu>
2021-12-28 10:00:45 -0500

    adding some more stuff

--- chatbox.py
+++ chatbox.py
@@ -5,3 +5,9 @@ my_bot Chat_bot (names "PyBot, read only-True, logic adapters= ['chatterbot.logi
 small_talk=['hi there!', "how are you?", "what are you doing today", "glad to hear your doing good", "we should grab lunch sometime."]
 talk1=["This is the best day ever", "This is my first attempt at chatterbot"] list trainer list_trainer (my_bot)
 
+for item in (small_talk, talk1):
+    list.trainer(item)
+
+corpus trainer chatter_bot_corpus_trainer(my_bot)
+coprus_trainer.treain('chatterbot.corpus.english')
+
