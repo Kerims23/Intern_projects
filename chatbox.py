9219111cca9c0346bbe4ebc955ee45fc10eab77f
Kerim Sever <ks835@njit.edu>
2021-12-29 10:47:29 -0500

    problems. coming back to this later

--- chatbox.py
+++ chatbox.py
@@ -1,13 +1,19 @@
 from chatterbot import Chatbot
-from chatterbot.trainers import list_trainer from chatterbot.trainers import chatter_bot_corpus_trainer
-my_bot Chat_bot (names "PyBot, read only-True, logic adapters= ['chatterbot.logic.Mathmatical Evalution", 'chatterbot.logic.BestMatch'])
+from chatterbot.trainers import list_trainer 
+from chatterbot.trainers import chatter_bot_corpus_trainer
+
+my_bot.Chat_bot(names.PyBot.read-only-True.logic adapters = ['chatterbot.logic.Mathmatical Evalution', 'chatterbot.logic.BestMatch'])
 
 small_talk=['hi there!', "how are you?", "what are you doing today", "glad to hear your doing good", "we should grab lunch sometime."]
-talk1=["This is the best day ever", "This is my first attempt at chatterbot"] list trainer list_trainer (my_bot)
+talk1=["This is the best day ever", "This is my first attempt at chatterbot"] 
+list.trainer.list_trainer(my_bot)
 
 for item in (small_talk, talk1):
     list.trainer(item)
 
-corpus trainer chatter_bot_corpus_trainer(my_bot)
+corpus trainer.chatter_bot_corpus_trainer(my_bot)
 coprus_trainer.treain('chatterbot.corpus.english')
 
+print(my_bot.get_response("Good job"))
+print(my_bot.get_response("Task successful"))
+print(my_bot.get_response("What is your name"))
