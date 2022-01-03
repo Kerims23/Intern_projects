47e9c10dafb20f416d3f8266575c55553e15b703
Kerim Sever <ks835@njit.edu>
2022-01-03 01:14:55 -0500

    commmit

--- excel.py
+++ excel.py
@@ -8,3 +8,6 @@ def process_workbook(filename):
     for row in range(2, sheet.max_row+1):
         cell=sheet.cell(row,3)
         corrected_price=float(cell.value.replace('$',''))*0.9
+        corrected_price_cell=sheet.cell(row,4)
+        corrected_price_cell.value=corrected_price
+
