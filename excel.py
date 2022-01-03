282c38ec5277721a2404742de4cd5e4d37c9d671
Kerim Sever <ks835@njit.edu>
2022-01-03 01:15:37 -0500

    commit

--- excel.py
+++ excel.py
@@ -2,12 +2,12 @@ import openpyxl as xl
 from openpyxl.chart import bar_chart, Reference
 
 def process_workbook(filename):
-    wb=xl.load_workbook(filename)
-    sheet=wb['sheet1']
+    wb = xl.load_workbook(filename)
+    sheet = wb['sheet1']
 
     for row in range(2, sheet.max_row+1):
-        cell=sheet.cell(row,3)
+        cell = sheet.cell(row,3)
         corrected_price=float(cell.value.replace('$',''))*0.9
-        corrected_price_cell=sheet.cell(row,4)
-        corrected_price_cell.value=corrected_price
+        corrected_price_cell = sheet.cell(row,4)
+        corrected_price_cell.value = corrected_price
 
