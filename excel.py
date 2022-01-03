12b38218f854d00bf903aa3e209791311c3faba9
Kerim Sever <ks835@njit.edu>
2022-01-03 01:16:54 -0500

    commit

--- excel.py
+++ excel.py
@@ -6,6 +6,7 @@ def process_workbook(filename):
     wb = xl.load_workbook(filename)
     sheet = wb['sheet1']
 
+#adding a for loop that goes through the file
     for row in range(2, sheet.max_row+1):
         cell = sheet.cell(row,3)
         corrected_price=float(cell.value.replace('$',''))*0.9
