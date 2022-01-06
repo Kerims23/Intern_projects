a0853c092b9d5b981718a1f09fbd965e223f416f
Kerim Sever <ks835@njit.edu>
2022-01-06 00:50:53 -0500

    blocklist

--- time.py
+++ time.py
@@ -4,3 +4,7 @@ from datetime import datetime as dt
 #windows host file
 hosts_paths=r"C:\Windows\Systems32\drivers\etc\hosts"
 redirect="127.0.0.1"
+#websites in blocklist
+websites=["www.yahoo.com","yahoo.com"]
+
+  
