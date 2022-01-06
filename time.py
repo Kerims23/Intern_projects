a6d2c8e3b6aed83b91beea765d6a2ec76e970fcd
Kerim Sever <ks835@njit.edu>
2022-01-06 00:51:23 -0500

    spacing

--- time.py
+++ time.py
@@ -1,11 +1,14 @@
 #libraries
 import time 
 from datetime import datetime as dt
+
 #windows host file
 hosts_paths=r"C:\Windows\Systems32\drivers\etc\hosts"
 redirect="127.0.0.1"
+
 #websites in blocklist
 websites=["www.yahoo.com","yahoo.com"]
+
 #start whoile loop
 while True:
   
