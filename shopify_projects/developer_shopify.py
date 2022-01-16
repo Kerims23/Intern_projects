cf6624022913e7bf4ffdbbab11f314d56c44a02f
Kerim Sever <ks835@njit.edu>
2022-01-16 12:53:29 -0500

    emp

--- shopify_projects/developer_shopify.py
+++ shopify_projects/developer_shopify.py
@@ -9,6 +9,7 @@ def convert_to_binary_data(filename):
 def insert_blob(emp_id, name, biodata_file):
     print("Insert Blob into python_employee table")
     try:
-        connection=mysql.connector.connect(host='localhost', database='python_db', user='pynative', password='pynative@#29')
-        cursor=connection.cursor()
-        sql_inter_blob_query="""INSERT INTO python_employee (id, name, photo, biodata) Values (%s,%s,%s,%s)"""
+        connection = mysql.connector.connect(host='localhost', database='python_db', user='pynative', password='pynative@#29')
+        curso = connection.cursor()
+        sql_inter_blob_query = """INSERT INTO python_employee (id, name, photo, biodata) Values (%s,%s,%s,%s)"""
+        emp_picture = convert_to_binary_data(photo)
