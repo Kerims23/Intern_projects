9276d8b329189a93add00c265d425e2ecadbb035
Kerim Sever <ks835@njit.edu>
2022-01-17 11:30:25 -0500

    edits

--- shopify_projects/developer_shopify.py
+++ shopify_projects/developer_shopify.py
@@ -10,13 +10,13 @@ def insert_blob(emp_id, name, biodata_file):
     print("Insert Blob into python_employee table")
     try:
         connection = mysql.connector.connect(host='localhost', database='python_db', user='pynative', password='pynative@#29')
-        curso = connection.cursor()
+        cursor = connection.cursor()
         sql_inter_blob_query = """INSERT INTO python_employee (id, name, photo, biodata) Values (%s,%s,%s,%s)"""
         emp_picture = convert_to_binary_data(photo)
         file = convert_to_binary_data(biodata_file)
 
         #making data into tuple format
         insert_blob_tuple = (emp_id, name, emp_picture, file)
-        
+        result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
         
 
