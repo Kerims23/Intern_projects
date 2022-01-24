e1665600b7fccbe84c831b0b0cc4fb3d0f5a64eb
Kerim Sever <ks835@njit.edu>
2022-01-24 11:43:12 -0500

    some errors, will come back to in the future

--- location_tracker.py
+++ location_tracker.py
@@ -3,7 +3,7 @@ import folium
 
 g = geocoder.ip("me")
 my_address = g.latlng
-my_map = folium.map(location = my_address = zoom_start = 12)
+my_map = folium.map(location = my_address, zoom_start = 12)
 
 
 my_map.save("my_map.html")
