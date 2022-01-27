d16caa3a76f36051be8f6ba2aec3d5f862a676bb
Kerim Sever <ks835@njit.edu>
2022-01-27 10:57:35 -0500

    ADD

--- cartoon.py
+++ cartoon.py
@@ -8,4 +8,4 @@ img = cv2.imread("img.png")
 #edges 
 gray = cv2.cvt Color (img, cv2.COLOR_BGR2GRAY)
 gray = cv2.median_blur (gray, 5)
-edges = cv2.adaptive Threshold (gray, 255, )
+edges = cv2.adaptive Threshold (gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, )
