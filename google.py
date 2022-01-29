2c59dcfee060c2595727c8f8431fa2fffc8d9dcc
Kerim Sever <ks835@njit.edu>
2022-01-29 11:30:26 -0500

    typo

--- google.py
+++ google.py
@@ -3,7 +3,7 @@ def jump(self, A):
     count = 0 
     pos = 0 
     for i in range(1,l):
-        A[i] = max(i+A[i],A[i-l])
+        A[i] = max(i+A[i], A[i-1])
     while(pos < l-1):
         if(pos >= A[pos]):
             return -1
