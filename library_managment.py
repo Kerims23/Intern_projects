5c7c9061aa3d9d81f151363d42a94f7987618197
Kerim Sever <ks835@njit.edu>
2022-02-15 10:59:46 -0500

    comment

--- library_managment.py
+++ library_managment.py
@@ -1,6 +1,6 @@
 from dbm.ndbm import library
 
-
+#we are making a class to get books and initilze it compared to the last project
 class library:
     def __init__(self, list_of_books):
         self.books = list_of_books
