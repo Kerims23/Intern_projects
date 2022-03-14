62284ed1f649ebe53170a5318e902d55227d3cbe
Kerim Sever <ks835@njit.edu>
2022-03-14 09:25:55 -0400

    creating routes

--- Flask_web_app/website/__init__.py
+++ Flask_web_app/website/__init__.py
@@ -4,5 +4,12 @@ def create_app():
     app = Flash(__name__)
     app.config['SECRETE_KEY'] = "This is the key"
 
+    from .views import views
+    from .auth import auth
+
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+
     return app
-    
